06e930c7a3d7921f7379216edf325daf16191c67
Chris Jewell <chrisjohnjewell@gmail.com>
2018-06-16 15:42:59 +1000

    Allow CORS

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,12 @@ module AppreciationBoard
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: %i[get post options]
+      end
+    end
   end
 end
